a671e5f54f3f10bdfd9ec9b406cc6bf9072a9576
Adam Lloyd <adam@alloy-d.net>
2014-08-01 16:59:50 -0400

    Configure curl and don't pull ALL the scripts into application.js.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,18 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require curl
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require_tree .
+
+curl({
+  baseUrl: "/assets",
+  packages: [
+    {
+      name: 'msgs',
+      location: 'msgs',
+      main: 'msgs',
+    }
+  ]
+});
