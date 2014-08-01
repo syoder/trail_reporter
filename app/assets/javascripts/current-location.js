18614c0eb7fc6b094d775aa2d2532c99b43519a2
Adam Lloyd <adam@alloy-d.net>
2014-08-01 17:02:27 -0400

    Send the current location over the message bus.

--- app/assets/javascripts/current-location.js
+++ app/assets/javascripts/current-location.js
@@ -1,20 +1,40 @@
-(function () {
+define(["message-bus"], function (bus) {
   "use strict";
   var watchID;
+  var exports = {};
+
+  bus.channel("browser-geolocation");
+  bus.transformer(function (message) {
+    return [message.coords.latitude, message.coords.longitude];
+  }, {
+    input: "browser-geolocation",
+    output: "current-coordinates",
+  });
 
   var success = function (pos) {
     window.LastPosition = pos;
     console.log("your current coordinates:", pos.coords.latitude, pos.coords.longitude)
+    bus.send("browser-geolocation", pos);
   }
   var failure = function (err) {
     console.log("error getting current location:", err)
   }
-  if (!!navigator.geolocation) {
-    if (!watchID) {
-      watchID = navigator.geolocation.watchPosition(success, failure, {
-        enableHighAccuracy: true,
-        maximumAge: 0
-      });
+
+  exports.startTracking = function () {
+    if (!!navigator.geolocation) {
+      if (!watchID) {
+        watchID = navigator.geolocation.watchPosition(success, failure, {
+          enableHighAccuracy: true,
+          maximumAge: 0
+        });
+      }
     }
+  };
+
+  exports.stopTracking = function () {
+    navigator.geolocation.clearWatch(watchID);
+    watchID = null;
   }
-}());
+
+  return exports;
+})
