f15d01e958aa339088ae138ddb0da045750016fa
maryhipp <maryhipp@gmail.com>
2014-08-01 13:16:05 -0400

    tags table"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :tags
+
   resources :comments
 
   resources :trails
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140801171058) do
+ActiveRecord::Schema.define(version: 20140801171510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 20140801171058) do
     t.integer  "category_id"
     t.integer  "trail_id"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
