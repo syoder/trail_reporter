87115b7ec53648af55911cc6baa064a17f7672f9
RJ Beers <rj.beers@opower.com>
2014-08-01 16:18:52 -0400

    added initial rspec tests for report

---
+++ spec/views/trails/edit.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "trails/edit", :type => :view do
+  before(:each) do
+    @trail = assign(:trail, Trail.create!())
+  end
+
+  it "renders the edit trail form" do
+    render
+
+    assert_select "form[action=?][method=?]", trail_path(@trail), "post" do
+    end
+  end
+end
