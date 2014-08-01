d1623d909a2c10c63d342f32a919a533d56d9293
maryhipp <maryhipp@gmail.com>
2014-08-01 13:09:32 -0400

    trails table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :trails
+
   resources :categories
 
   resources :reports
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140801170158) do
+ActiveRecord::Schema.define(version: 20140801170851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20140801170158) do
     t.integer  "category_id"
     t.integer  "trail_id"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "trails", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
