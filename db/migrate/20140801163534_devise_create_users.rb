dcb18bb0ad16885ca91bf9468386aab1e96c3b98
MichaelByrd <michaelbyrdsemail@gmail.com>
2014-08-01 12:43:12 -0400

    Modified the create_users migration, added a field for :trail_name :string

--- db/migrate/20140801163534_devise_create_users.rb
+++ db/migrate/20140801163534_devise_create_users.rb
@@ -30,6 +30,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      # Trail-name
+      t.string :trail_name
 
       t.timestamps
     end
