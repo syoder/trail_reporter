7a15aa81feec1f7a0360523ad4eb23fda7355e47
Adam Lloyd <adam@alloy-d.net>
2014-08-03 13:58:58 -0400

    Make the navbar collapsible.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require twitter/bootstrap/bootstrap-collapse
 //= require bootstrap-tagsinput
 //= require current-location
