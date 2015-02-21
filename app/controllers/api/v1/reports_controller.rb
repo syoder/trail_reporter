bb2c001b5c88f17425083df9139763e995b48045
Scott Yoder <yodersb@gmail.com>
2015-02-21 17:02:22 -0500

    allow creating new reports via api

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ReportsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  before_action :authenticate_user!, only: [:create, :update, :destroy]
 
   # CURL example:
   #   curl "localhost:3000/api/v1/reports?radius_miles=5&lat38.835161=&lng=-77.311821"
@@ -7,7 +9,38 @@ class Api::V1::ReportsController < ApplicationController
     @reports = Report.within(params[:radius_miles], origin: filter_location)
   end
 
+  # CURL example (doesn't include auth headers!):
+=begin
+     curl -i \
+       -H "Content-Type: application/json" \
+       -X POST -d '{"reports": [{"latitude":38.835161,"longitude":-77.311821,"description":"created via curl"}]}' \
+       "localhost:3000/api/v1/reports"
+=end
+
+  def current_user
+    User.first
+  end
+
+  def create
+    @reports = reports_params.map {|p| current_user.reports.build(p)}
+    success = @reports.map {|report| report.save }
+    if success.all?
+      head :no_content
+    else
+      render json: {reports: @reports.map{|report| report.slice(:id, :latitude, :longitude, :errors)}},
+        status: :unprocessable_entity
+    end
+  end
+
   private
+
+    def reports_params
+      params[:reports].map {|p| report_params(p)}
+    end
+
+    def report_params(params_hash)
+      params_hash.permit(:latitude, :longitude, :description, :reported_at, :category_id, :trail_id)
+    end
 
     def filter_location
       [params[:lat].to_f, params[:lng].to_f]
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :reports, only: [:index]
+      resources :reports, only: [:index, :create]
     end
   end
 
