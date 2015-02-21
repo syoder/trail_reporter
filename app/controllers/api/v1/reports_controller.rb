09b252ccd1dca262e8e795155502e434258db224
Scott Yoder <yodersb@gmail.com>
2015-02-21 16:17:08 -0500

    filter reports based on a lat/lng

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::ReportsController < ApplicationController
 
+  # CURL example:
+  #   curl "localhost:3000/api/v1/reports?radius_miles=5&lat38.835161=&lng=-77.311821"
+  #
   def index
-    # TODO: filter by distance via lat/lng
-    @reports = Report.all
+    @reports = Report.within(params[:radius_miles], origin: filter_location)
   end
+
+  private
+
+    def filter_location
+      [params[:lat].to_f, params[:lng].to_f]
+    end
 end
